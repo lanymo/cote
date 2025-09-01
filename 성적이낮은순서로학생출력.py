e824a2df37bcebc46fd22e28f1777119378e4af8
lanymo <mien4517@gmail.com>
2025-09-01 11:43:08 +0900

    sorting ps

---
+++ 성적이낮은순서로학생출력.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+
+array = []
+for i in range(n):
+    data = input().split()
+    array.append((data[0], int(data[1])))
+
+array.sort(key = lambda x: x[1])
+
+for i in array:
+    print(i[0], end = ' ')
