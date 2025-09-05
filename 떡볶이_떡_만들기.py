11b99c9ab4af2787a3b03c779697f2e684b77c4e
lanymo <mien4517@gmail.com>
2025-09-06 00:12:48 +0900

    first commit: 해설 없이 정답 도출

---
+++ 떡볶이_떡_만들기.py
@@ -0,0 +1,26 @@
+# 해설 없이 짠 코드
+
+#n: 떡의 개수, m: 요청한 떡의 길이
+n, m = map(int, input().split())
+tteoks = list(map(int, input().split()))
+
+tteoks.sort()
+start = 0
+end = tteoks[n-1] -1
+
+while(start <= end):
+    left_sum = 0
+    mid = (start + end) // 2
+    for t in tteoks:
+        if(t > mid):
+            left_sum += (t - mid)
+    if (left_sum < m):
+        end = mid -1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
+
+
+
