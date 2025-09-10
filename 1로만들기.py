ce3169e2de98410165abeadcb3d0ab250471c478
lanymo <mien4517@gmail.com>
2025-09-10 17:09:38 +0900

    dp_second_commit_first_ex_catch the error

--- 1로만들기.py
+++ 1로만들기.py
@@ -6,7 +6,7 @@ for i in range(2, n+1): # bottom up 방식으로 dp
     d[i] = d[i-1] + 1
     if ( i%2 == 0):
         d[i] = min(d[i], d[i//2] + 1)
-    elif (i % 3 == 0):
+    if (i % 3 == 0):
         d[i] = min(d[i], d[i//3] + 1)
 
 print(d[n])
